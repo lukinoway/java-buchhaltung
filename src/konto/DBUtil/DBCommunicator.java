9376d75cc82ad25acb441dc286f61712026139ae
unknown <lukas.pichler@gmail.com>
2015-10-24 17:05:46 +0200

    implemented new update and delete function

--- src/konto/DBUtil/DBCommunicator.java
+++ src/konto/DBUtil/DBCommunicator.java
@@ -42,9 +42,10 @@ public class DBCommunicator {
 	  }
 	  
 	  /**
-	   * INSERT new data to DB
-	 * @return 
-	   * @return
+	   * Function to insert new Data to DB
+	   * @param tablename
+	   * @param valuepart
+	   * @param columnpart
 	   * @throws Exception
 	   */
 	  public void insertData(String tablename, String valuepart, String columnpart ) throws Exception {
@@ -57,12 +58,65 @@ public class DBCommunicator {
 			      // Statements allow to issue SQL queries to the database
 			      statement = connect.createStatement();
 			      
-			      // Result set get the result of the SQL query
 			      // print query
 			      System.out.println("insert into konto_app." + tablename + "( " + columnpart + " ) values( " + valuepart + " )");
 			      statement.executeUpdate("insert into konto_app." + tablename + "( " + columnpart + " ) values( " + valuepart + " )");
-			      //writeResultSet(resultSet);
-			      //return resultSet;
+
+			    } catch (Exception e) {
+			      throw e;
+			    } finally {
+			      //close();
+			    }
+	  }
+	  
+	  /**
+	   * Function to update data in DB
+	   * @param tablename
+	   * @param setpart
+	   * @param wherepart
+	   * @throws Exception
+	   */
+	  public void updateData(String tablename, String setpart, String wherepart ) throws Exception {
+		    try {
+			      // This will load the MySQL driver, each DB has its own driver
+			      Class.forName("com.mysql.jdbc.Driver");
+			      // Setup the connection with the DB
+			      connect = DriverManager.getConnection("jdbc:mysql://" + server_name + "/konto_app?"+"user=" + db_user + "&password=" + db_pwd);
+
+			      // Statements allow to issue SQL queries to the database
+			      statement = connect.createStatement();
+			      
+			      // print query
+			      System.out.println("update konto_app." + tablename + "set " + setpart + " where " + wherepart);
+			      statement.executeUpdate("update konto_app." + tablename + "set " + setpart + " where " + wherepart);
+
+			    } catch (Exception e) {
+			      throw e;
+			    } finally {
+			      //close();
+			    }
+	  }
+	  
+	  /**
+	   * Function to delete data from DB
+	   * @param tablename
+	   * @param wherepart
+	   * @throws Exception
+	   */
+	  public void deleteData(String tablename, String wherepart ) throws Exception {
+		    try {
+			      // This will load the MySQL driver, each DB has its own driver
+			      Class.forName("com.mysql.jdbc.Driver");
+			      // Setup the connection with the DB
+			      connect = DriverManager.getConnection("jdbc:mysql://" + server_name + "/konto_app?"+"user=" + db_user + "&password=" + db_pwd);
+
+			      // Statements allow to issue SQL queries to the database
+			      statement = connect.createStatement();
+			      
+			      // print query
+			      System.out.println("delete from konto_app." + tablename + " where " + wherepart);
+			      statement.executeUpdate("delete from konto_app." + tablename + " where " + wherepart);
+
 			    } catch (Exception e) {
 			      throw e;
 			    } finally {
