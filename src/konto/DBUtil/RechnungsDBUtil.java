29ee36fd41708f30193edc3c5001332e2d75e576
Lukas Pichler <lukas.pichler@gmail.com>
2015-12-02 16:04:12 +0100

    rewrote Bill DB Util

--- src/konto/DBUtil/RechnungsDBUtil.java
+++ src/konto/DBUtil/RechnungsDBUtil.java
@@ -6,166 +6,21 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.sql.Blob;
-import java.sql.Connection;
 import java.sql.Date;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import konto.model.Rechnung;
-import konto.model.Transaktion;
-import konto.view.TransaktionOverviewController;
 
-public class RechnungsDBUtil extends DBCommunicator{
+public class RechnungsDBUtil extends DBCommunicator implements IRechnung{
 	
-	// main stuff
-	private Connection connect = null;
-	private Statement statement = null;
-	private ResultSet resultSet = null;
-	
-	private Rechnung rechnung;
-	private TransaktionOverviewController troc;
-	private ObservableList<Rechnung> rechnungsData = FXCollections.observableArrayList();
+	private ResultSet resSet = null;
 	
 	public RechnungsDBUtil() {
 		super();
-	}
-	
-	/**
-	 * Load unknown Bills to view
-	 */
-	public void loadRechnungsPool() {
-		try {
-			// first of all clear data if there is any
-			rechnungsData.removeAll(this.rechnung);
-			troc.RechnungsPoolTable.setItems(getRechnungsData());
-		
-			this.resultSet = getData("db_transaktion_rechnung",
-									 "transaktions_anhang_id, created, transaktions_anhang_text",
-									 "where transaktions_detail_id IS null and transaktions_id IS null");
-			
-			// fill table
-			while(this.resultSet.next()) {
-				int billId = this.resultSet.getInt(1);
-				String billDate = this.resultSet.getString(2);
-				String billText = this.resultSet.getString(3);
-				
-				System.out.print("ID:" + billId + " / Date:" + billDate + " / Text:" + billText);
-				rechnungsData.add(new Rechnung(billId, LocalDate.parse(billDate), billText));
-				
-				troc.rechnungsId.setCellValueFactory(cellDate -> cellDate.getValue().RechnungsIdProperty());
-				troc.rechnungsDatum.setCellValueFactory(cellDate -> cellDate.getValue().RechnungsDatumProperty());
-				troc.rechnungsText.setCellValueFactory(cellDate -> cellDate.getValue().RechnungsTextProperty());
-			}
-			troc.RechnungsPoolTable.setItems(getRechnungsData());
-			
-		} catch (NullPointerException e) {
-			System.out.println("keine Daten im Pool vorhanden!");
-		} catch (Exception e) {
-			System.out.println("Hier lief was schief - loadRechnungsPool");
-			e.printStackTrace();
-		} finally {
-			close();
-		}
-	}
-	
-	public void attachBilltoPool(File rechnung, String billText) {
-		try {
-		    // prepared Statement execution, otherwise we are not able to load BLOB
-			Class.forName("com.mysql.jdbc.Driver");
-			connect = DriverManager.getConnection("jdbc:mysql://" + server_name + "/konto_app?"+"user=" + db_user + "&password=" + db_pwd);
-
-		    String pSql = "insert into konto_app.db_transaktion_rechnung"
-		    			+ "(transaktions_anhang, transaktions_anhang_filetype, created, transaktions_anhang_text )"
-		    			+ "values(?,?,?,?)" ;
-		    
-		    PreparedStatement pStmt = connect.prepareStatement((pSql), Statement.RETURN_GENERATED_KEYS);
-		    
-		    InputStream rechnungStream = new BufferedInputStream( new FileInputStream(rechnung));
-		    System.out.println("Filesize: " + rechnung.length());
-		    pStmt.setBinaryStream(1, rechnungStream, rechnungStream.available());
-		    
-		    // store filetype in a sperate field
-		    pStmt.setString(2, getFileExtension(rechnung));
-		    
-		    pStmt.setDate(3, Date.valueOf(LocalDate.now()));
-		    pStmt.setString(4, billText);
-		    pStmt.executeUpdate();
-		    
-		    // get transaktions_anhang_id from resultset
-		    ResultSet rs = pStmt.getGeneratedKeys();
-		    rs.next();
-		    System.out.println("Added bill to DB, RechnungsNR: " + rs.getInt(1));
-
-		} catch (Exception e) {
-			System.out.println("attachBill - Fehler trat auf");
-			e.printStackTrace();
-		} finally {
-			close();
-		}
-	}
-	
-	/**
-	 * This function will download the data from DB
-	 * @param trd_id
-	 * @return 
-	 */
-	public String downloadBillFromPool(int billId) {
-		String filepath = null;
-		try {
-			this.resultSet = getData("db_transaktion_rechnung", 
-									 "transaktions_anhang_id, transaktions_anhang_filetype, transaktions_anhang", 
-									 "where transaktions_anhang_id =" + billId);
-
-			this.resultSet.next();
-			
-			int tra_id = this.resultSet.getInt(1);
-			String fileext = this.resultSet.getString(2);
-			Blob blob = this.resultSet.getBlob(3);
-			
-			String filename = "Rechnung_" + tra_id + "_";
-						
-			// write some log output
-			System.out.println("Read " + blob.length() + " bytes");
-			
-			byte[] buff = blob.getBytes(1, (int) blob.length());
-			File output = File.createTempFile(filename, fileext, new File("C:\\Windows\\Temp"));
-			FileOutputStream out = new FileOutputStream(output);
-			out.write(buff);
-			out.close();
-			
-			System.out.println("created File:" + output.getAbsolutePath());
-			filepath = output.getAbsolutePath();
-			
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			close();
-		}
-		return filepath;
-	}
-	
-	/**
-	 * Delete Entry from Pool
-	 * @param billId
-	 */
-	public void deleteBillFromPool (int billId) {
-		try {
-			deleteData("db_transaktion_rechnung", "transaktions_anhang_id =" + billId);
-		} catch (NullPointerException e) {
-			System.out.println("Es konnten keine Daten gefunden werden");
-		} catch (Exception e) {
-			System.out.println("Hier lief was shief - deleteBillFromPool");
-			e.printStackTrace();
-		} finally {
-			close();
-		}
 	}
 	
 	public void linkBillToTransaktion(int tr_id, int billId) throws Exception {
@@ -207,25 +62,135 @@ public class RechnungsDBUtil extends DBCommunicator{
 		}
 	}
 	
-	// You need to close the resultSet
-	private void close() {
+	
+	/**
+	 * insert new Rechnung to DB
+	 */
+	public void insertRechnung(Rechnung rechnung) {
 		try {
-			if (resultSet != null) {
-				resultSet.close();
-			}
-			if (this.resultSet != null) {
-				this.resultSet.close();
-			}
-		    if (statement != null) {
-			    statement.close();
-			}
-		    if (connect != null) {
-			    connect.close();
-			}
+		    // prepared Statement execution, otherwise we are not able to load BLOB
+			super.connectDB();
+
+		    String pSql = "insert into konto_app.db_transaktion_rechnung"
+		    			+ "(transaktions_anhang, transaktions_anhang_filetype, created, transaktions_anhang_text )"
+		    			+ "values(?,?,?,?)" ;
+		    
+		    PreparedStatement pStmt = super.connect.prepareStatement((pSql), Statement.RETURN_GENERATED_KEYS);
+		    
+		    InputStream rechnungStream = new BufferedInputStream( new FileInputStream(rechnung.getRechnung()));
+		    System.out.println("Filesize: " + rechnung.getRechnung().length());
+		    pStmt.setBinaryStream(1, rechnungStream, rechnungStream.available());
+		    
+		    // store filetype in a sperate field
+		    pStmt.setString(2, getFileExtension(rechnung.getRechnung()));
+		    
+		    pStmt.setDate(3, Date.valueOf(LocalDate.now()));
+		    pStmt.setString(4, rechnung.getRechnungsText());
+		    pStmt.executeUpdate();
+		    
+		    // get transaktions_anhang_id from resultset
+		    resSet = pStmt.getGeneratedKeys();
+		    resSet.next();
+		    System.out.println("Added bill to DB, RechnungsNR: " + resSet.getInt(1));
+		    
+		    if (this.resSet != null) {
+				this.resSet.close();
+		    }
+		} catch (Exception e) {
+			System.out.println("attachBill - Fehler trat auf");
+			e.printStackTrace();
+		} finally {
+			super.close();
+		}
+	}
+
+	@Override
+	public void updateRechnung(Rechnung rechnung) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteRechnung(int billId) {
+		try {
+			deleteData("db_transaktion_rechnung", "transaktions_anhang_id =" + billId);
+		} catch (NullPointerException e) {
+			System.out.println("Es konnten keine Daten gefunden werden");
+		} catch (Exception e) {
+			System.out.println("Hier lief was shief - deleteBillFromPool");
+			e.printStackTrace();
+		} finally {
+			super.close();
+		}
+	}
+
+	public String downloadRechnung(int billId) {
+		String filepath = null;
+		try {
+			this.resSet = getData("db_transaktion_rechnung", 
+									 "transaktions_anhang_id, transaktions_anhang_filetype, transaktions_anhang", 
+									 "where transaktions_anhang_id =" + billId);
+
+			this.resSet.next();
+			
+			int tra_id = this.resSet.getInt(1);
+			String fileext = this.resSet.getString(2);
+			Blob blob = this.resSet.getBlob(3);
+			
+			String filename = "Rechnung_" + tra_id + "_";
+						
+			// write some log output
+			System.out.println("Read " + blob.length() + " bytes");
+			
+			byte[] buff = blob.getBytes(1, (int) blob.length());
+			File output = File.createTempFile(filename, fileext, new File("C:\\Windows\\Temp"));
+			FileOutputStream out = new FileOutputStream(output);
+			out.write(buff);
+			out.close();
+			
+			System.out.println("created File:" + output.getAbsolutePath());
+			filepath = output.getAbsolutePath();
+			
+		    if (this.resSet != null) {
+				this.resSet.close();
+		    }	
 		} catch (Exception e) {
 			e.printStackTrace();
-		}	
-    } 
+		} finally {
+			close();
+		}
+		return filepath;
+	}
+	
+	public ArrayList<Rechnung> selectDataFromPool() {
+		ArrayList<Rechnung> rechnungsCollector = new ArrayList<Rechnung>();
+		try {
+			this.resSet = getData("db_transaktion_rechnung",
+									 "transaktions_anhang_id, created, transaktions_anhang_text",
+									 "where transaktions_detail_id IS null and transaktions_id IS null");
+			
+			// fill table
+			while(this.resSet.next()) {
+				int billId = this.resSet.getInt(1);
+				String billDate = this.resSet.getString(2);
+				String billText = this.resSet.getString(3);
+				
+				System.out.print("ID:" + billId + " / Date:" + billDate + " / Text:" + billText);
+				rechnungsCollector.add(new Rechnung(billId, LocalDate.parse(billDate), billText));
+			}
+			
+		    if (this.resSet != null) {
+				this.resSet.close();
+		    }	
+		} catch (NullPointerException e) {
+			System.out.println("keine Daten im Pool vorhanden!");
+		} catch (Exception e) {
+			System.out.println("Hier lief was schief - loadRechnungsPool");
+			e.printStackTrace();
+		} finally {
+			super.close();
+		}
+		return rechnungsCollector;
+	}
 	
 	/** 
 	 * This function is used to get the extension
@@ -237,18 +202,5 @@ public class RechnungsDBUtil extends DBCommunicator{
         if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
         return fileName.substring(fileName.lastIndexOf("."));
         else return "";
-    }
-	
-    public ObservableList<Rechnung> getRechnungsData() {
-        return rechnungsData;
-    }
-	
-    /**
-     * Is called by the main application to give a reference back to itself.
-     *
-     * @param mainApp
-     */
-    public void setController(TransaktionOverviewController troc) {
-        this.troc = troc;
     }
 }
