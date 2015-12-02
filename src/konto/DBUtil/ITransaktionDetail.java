0019554d24169a446a33407d96717d5b2083fb39
Lukas Pichler <lukas.pichler@gmail.com>
2015-12-02 16:42:16 +0100

    added interface

---
+++ src/konto/DBUtil/ITransaktionDetail.java
@@ -0,0 +1,18 @@
+package konto.DBUtil;
+
+import java.util.ArrayList;
+
+import konto.model.Transaktion;
+import konto.model.TransaktionDetail;
+
+public interface ITransaktionDetail {
+
+	public void insertDetail(TransaktionDetail detail);
+	
+	public void updateDetail(TransaktionDetail detail);
+	
+	public void deleteDetail(int detailId);
+	
+	public ArrayList<TransaktionDetail> selectDetail(Transaktion transaktion);
+	
+}
