ee5a295c54f1ee545818df89842934ba3184e9e6
Pichler Lukas <lpichle@wamas.com>
2015-12-04 13:57:44 +0100

    add

---
+++ src/konto/MenuBar.java
@@ -0,0 +1,17 @@
+package konto;
+
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+public class MenuBar extends VerticalLayout {
+	
+	Label header = new Label("KontoAPP");
+	
+	public MenuBar() {
+		
+		header.setStyleName("h1");
+		this.addComponent(header);
+		this.setWidth(150, Unit.PIXELS);
+	}
+
+}
