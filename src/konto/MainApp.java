f37f74a4fe25fb64ba7e73b6695c8a899af8ddea
Pichler Lukas <lpichle@wamas.com>
2015-12-04 13:57:04 +0100

    src/konto/MenuBar.java

--- src/konto/MainApp.java
+++ src/konto/MainApp.java
@@ -1,6 +1,9 @@
 package konto;
 
 import com.vaadin.server.VaadinRequest;
+import konto.MenuBar;
+import konto.view.TransaktionsTable;
+
 import com.vaadin.ui.*;
 
 
@@ -9,7 +12,17 @@ public class MainApp extends UI {
     @Override
     protected void init(VaadinRequest request) {
         VerticalLayout view = new VerticalLayout();
-        view.addComponent(new Label("Hello Vaadin!"));
+        VerticalLayout view2 = new VerticalLayout();
+        //view.addComponent(new Label("Hello Vaadin!"));
+        MenuBar menu = new MenuBar();
+        view.addComponent(menu);
+        
+        TransaktionsTable tab = new TransaktionsTable();
+        tab.setSizeFull();
+        
+        view2.addComponent(tab);
+        view.addComponent(view2);
+        
         setContent(view);
     }
 }
