85c20050eb5059ae94e23e51b71e3d005217f6fb
Pichler Lukas <lpichle@wamas.com>
2015-12-15 10:22:50 +0100

    cleanup in mainapp

--- src/konto/MainApp.java
+++ src/konto/MainApp.java
@@ -1,45 +1,27 @@
 package konto;
 
-import java.security.NoSuchAlgorithmException;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-
-import org.jsoup.select.Collector;
-
-import com.vaadin.data.Container;
-import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.VaadinRequest;
 import konto.MenuBar;
-import konto.DBUtil.ITransaktion;
-import konto.DBUtil.TransaktionDBUtil;
-import konto.model.Transaktion;
-import konto.view.TransaktionsGrid;
-import konto.view.TransaktionsSearchBar;
-
+import konto.view.Transaktion.TransaktionsMainView;
 import com.vaadin.ui.*;
-import com.vaadin.ui.Button.ClickEvent;
 
 
 public class MainApp extends UI {
 
-	ITransaktion transaktion = new TransaktionDBUtil();
-    Container.Indexed indexed = null;
-    TransaktionsGrid tab = new TransaktionsGrid();
-    Panel panel = new Panel();
+	private static final long serialVersionUID = 1L;
+	//layout part
+	HorizontalLayout main = new HorizontalLayout();
+    VerticalLayout view = new VerticalLayout();
+    VerticalLayout view2 = new VerticalLayout();
+    MenuBar menu = new MenuBar();
+    TransaktionsMainView transaktionsView = new TransaktionsMainView();
 	
     @Override
     protected void init(VaadinRequest request) {
     	
-        VerticalLayout view = new VerticalLayout();
         view.setWidth(150, Unit.PIXELS);
-        VerticalLayout view2 = new VerticalLayout();
-        MenuBar menu = new MenuBar();
         view.addComponent(menu);
-        
 
-        
         // some test data
         /*
         try {
@@ -55,49 +37,9 @@ public class MainApp extends UI {
 			e.printStackTrace();
 		}
 		*/
-        
 
-        
-        TransaktionsSearchBar searchBar = new TransaktionsSearchBar();
-        searchBar.setWidth(100, Unit.PERCENTAGE);
-        
-        searchBar.searchBtn.addClickListener(new Button.ClickListener() {
-			
-			@Override
-			public void buttonClick(ClickEvent event) {
-				LocalDate fromDate = searchBar.getFromDate();
-				LocalDate toDate = searchBar.getToDate();
-				
-				System.out.println("ButtonClick - von: " + fromDate);
-				System.out.println("ButtonClick - bis: " + toDate);
-				
-				
-				ArrayList<Transaktion> test = transaktion.selectDataByDate(fromDate, toDate, 1);
-				System.out.println("ButtonClick - back from dbUtil");
-				indexed = new BeanItemContainer<>(Transaktion.class, test);
-				tab = new TransaktionsGrid(indexed);
-				tab.setWidth(100, Unit.PERCENTAGE);
-				panel.setContent(tab);
-			}
-		});
-        
-        tab.setWidth(100, Unit.PERCENTAGE);
-        
-    	VerticalLayout panelContent = new VerticalLayout();
-    	panelContent.addComponent(searchBar);
-    	panelContent.addComponent(tab);
-    	panel.setContent(panelContent);
-    	panel.setWidth(1000, Unit.PIXELS);
-        
-        HorizontalLayout layout = new HorizontalLayout(panel);
-        
         // Main View
-        layout.setComponentAlignment(panel, Alignment.MIDDLE_CENTER);
-        layout.setMargin(true);
-        layout.setExpandRatio(panel, 1.0f);
-        view2.addComponent(layout);
-        
-        HorizontalLayout main = new HorizontalLayout();
+        view2.addComponent(transaktionsView);
         
         main.addComponent(view);
         main.addComponent(view2);
